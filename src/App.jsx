247d504d09a9aea5e704ca828a3ba83787451921
aumnatthacha <aummumdaeng@gmail.com>
2024-08-26 15:31:34 +0700

    Unit Test and Component Test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./App.css";
+import BMICalculatorComponent from "./BMICalculatorComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <BMICalculatorComponent />
+    </div>
+  );
+}
+
+export default App;
